80ed3ebf58aaf26bd291c1eb5d99cdf2f57d07f9
fangkun119 <fangkun119@hotmail.com>
2019-06-09 20:41:52 -0700

    ch07 time series

--- start_jupyter_docker.sh
+++ start_jupyter_docker.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+
+#docker pull azurevolition/tensorflow:latest-py3-jupyter-lib
 docker run -it -p 8888:8888 -v $(pwd)/code:/tf/notebooks azurevolition/tensorflow:latest-py3-jupyter-lib
+
